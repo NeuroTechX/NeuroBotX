fac2e78489ba25ff137b687cb04b99a9cc754e29
Amine Abdessemed <amine.abdessemed@gmail.com>
2016-11-15 11:45:00 -0500

    Added msg on team join

--- server.js
+++ server.js
@@ -34,6 +34,9 @@ slapp.message('help', ['mention', 'direct_message'], (msg) => {
   msg.say(HELP_TEXT)
 })
 
+slapp.event('team_join', (msg) => {
+  msg.say('Woot woot')
+})
 // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
 slapp
   .message('^(hi|hello|hey)$', ['direct_mention', 'direct_message'], (msg, text) => {
