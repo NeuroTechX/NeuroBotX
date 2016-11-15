ff6a7336334a1c1fd61f60d8463f2c04d267b876
Amine Abdessemed <amine.abdessemed@gmail.com>
2016-11-15 12:48:21 -0500

    Var name

--- server.js
+++ server.js
@@ -8,6 +8,7 @@ const slack = require('slack')
 
 // use `PORT` env var on Beep Boop - default to 3000 locally
 var port = process.env.PORT || 3000
+var token =process.env.SLACK_VERIFY_TOKEN
 
 var slapp = Slapp({
   // Beep Boop sets the SLACK_VERIFY_TOKEN env var
@@ -36,8 +37,8 @@ slapp.message('help', ['mention', 'direct_message'], (msg) => {
 })
 
 slapp.event('team_join', (msg) => {
-  var res = slack.im.open(verify_token,msg.body.event.user)
-  slack.chat.postMessage(verify_token,res.body.channel.id,"Please visite www.neurotechedu.com");
+  var res = slack.im.open(token,msg.body.event.user)
+  slack.chat.postMessage(token,res.body.channel.id,"Please visite www.neurotechedu.com");
 })
 // "Conversation" flow that tracks state - kicks off when user says hi, hello or hey
 slapp
