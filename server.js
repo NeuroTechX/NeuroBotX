71e85412c506bd6e35bfd474165d87d0385e5f90
Amine Abdessemed <amine.abdessemed@gmail.com>
2017-01-09 11:37:49 -0500

    help and welcome

--- server.js
+++ server.js
@@ -23,15 +23,55 @@ var github = new GitHubApi({
 });
 
 var HELP_TEXT = `
-I will respond to the following messages:
-\`help\` - to see this message.
-\`stats\` - I will show you what people are talking about the most.
+If you are looking for help or have a question, the quickest way to get an answer is to message one of the directors. @yannick and @sydneyneurotechx are the more active Directors on slack.
+
+Here is a list of all the directors and their usernames:
+
+Executive Director: @yannick
+Operations Director: @sydneyneurotechx
+Communications Director: @lucia.gallardo
+Science Director:@ melanie
+
+If you are looking for online Neurotech  resources, check out NeuroTechEDU
+
+Link: http://www.neurotechedu.com/
+
+If you are looking for a chapter to get connected to, please explore the different chapter public channels. All city chapter channels have a “_” at the beginning of it.
+
+If you are looking for archived Slack Text, go to:
+
+https://github.com/NeuroTechX/ntx_slack_archive
 `
 var WELCOME_TEXT = `
-Welcome to NeuroTechX slack, I'm the EduBotX, I'm here to help you get started.
-If you want to learn about neurotechnologies, please visit www.neurotechedu.com
-If you want to now about the activities of NeuroTechX, please visit www.neurotechx.com
-Please introduce yourself on the #introductions channel.
+Welcome to the NeuroTechX Slack! This Slack brings together hundreds of people from across the world to communicate about NeuroTechnology. Everyone here is passionate about the domain and is willing to help out.
+
+Here is how you should get started:
+
+
+The first thing you should do is read our code of conduct. It can be found at: http://ntx-dockit.readthedocs.io/en/latest/Conduct.html
+
+The second thing you should do is go to the #introductions channel in introduce yourself! Everyone here is very friendly!
+
+The third thing you should do is send a message to the directors of NeuroTechX and say hello! Not everyone is active at all times but you should get a response from most of them. Here are their usernames:
+
+Executive Director: @yannick
+Operations Director: @sydneyneurotechx
+Communications Director: @lucia.gallardo
+Science Director:@ melanie
+
+
+If you are new to the domain of NeuroTechnology, we urge you to check out our NeurotechEDU page! There is a list of amazing resources that exist on it, that will be beneficial for you in helping to advance your knowledge in the domain. You will see how interesting it is to be able to find new ways to learn about the brain and how it work and what tools are available to you to do so. If you have questions related to the content, feel free to ask them in Slack
+
+Link: http://www.neurotechedu.com/
+
+Since NeurotechX is a bootstrapped non-profit, we are unable to pay for the premium version of Slack and therefore all messages after 10,000 are deleted. We have built a temporary solution by archiving all of the text onto our Github page.
+
+https://github.com/NeuroTechX/ntx_slack_archive
+
+
+
+
+Finally take a look at the different chapters that exist in NeuroTechX. You may discover that there is a local community for you to get connected to.  All city chapter channels have a “_” at the beginning of it.
 `
 var LINKS_REGEX = /(\b(https?|ftp|file|http):\/\/[-A-Z0-9+&@#\/%?=~_|!:,.;]*[-A-Z0-9+&@#\/%=~_|])/ig;
 
