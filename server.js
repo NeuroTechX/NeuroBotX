c763c3a2b70f7755d53484f8ce3c5516197a42cf
Amine Abdessemed <amine.abdessemed@gmail.com>
2017-01-20 12:28:48 -0500

    reset test 2

--- server.js
+++ server.js
@@ -27,7 +27,7 @@ var HELP_TEXT = `
 Hi there! I’m NeuroBotX, your NeuroTechX Slack bot. I currently only have 1 response when you send me a message, so here is everything you need to know about NeuroTechX and this Slack.
 
 
-If you are looking for help or have a question about NeuroTechX, the quickest way to get an answer is to message one of the directors. @yannick and @sydneyneurotechx are the more active Directors on slack. 
+If you are looking for help or have a question about NeuroTechX, the quickest way to get an answer is to message one of the directors. @yannick and @sydneyneurotechx are the more active Directors on slack.
 
 Here is a list of all the directors and their usernames:
 
@@ -40,7 +40,7 @@ If you are looking for online Neurotech  resources, check out NeuroTechEDU
 
 Link: http://www.neurotechedu.com/
 
-If you are looking for a chapter to get connected to, please explore the different Slack public chapter channels. All city chapters channels have a “_” at the beginning of it. 
+If you are looking for a chapter to get connected to, please explore the different Slack public chapter channels. All city chapters channels have a “_” at the beginning of it.
 
 To find a list of cities where we are located in, please visit http://neurotechx.com/ and see if there is a chapter near you!
 
@@ -49,7 +49,7 @@ If you are looking for archived Slack text, go to:
 https://github.com/NeuroTechX/ntx_slack_archive
 `
 var WELCOME_TEXT = `
-Welcome to the NeuroTechX Slack! This Slack brings together hundreds of people from across the world to communicate about Neurotechnology. Everyone here is passionate about the domain and is willing to help out. 
+Welcome to the NeuroTechX Slack! This Slack brings together hundreds of people from across the world to communicate about Neurotechnology. Everyone here is passionate about the domain and is willing to help out.
 
 Here is how you should get started:
 
@@ -70,12 +70,12 @@ If you are new to the domain of Neurotechnology, we urge you to check out our Ne
 
 Link: http://www.neurotechedu.com/
 
-Since NeurotechX is a bootstrapped non-profit, we are unable to pay for the premium version of Slack and therefore all messages after 10,000 are deleted. We have built a temporary solution by archiving all of the text onto our Github page. You can visit the list of archived text here. We only track public channels. 
+Since NeurotechX is a bootstrapped non-profit, we are unable to pay for the premium version of Slack and therefore all messages after 10,000 are deleted. We have built a temporary solution by archiving all of the text onto our Github page. You can visit the list of archived text here. We only track public channels.
 
 https://github.com/NeuroTechX/ntx_slack_archive
 
 
-Finally take a look at the different chapters that exist in NeuroTechX. You may discover that there is a local community for you to get connected to.  All city chapter channels have a “_” at the beginning of it. 
+Finally take a look at the different chapters that exist in NeuroTechX. You may discover that there is a local community for you to get connected to.  All city chapter channels have a “_” at the beginning of it.
 
 To find a list of cities where we are located in, please visit http://neurotechx.com/ and see if there is a chapter near you!
 `
@@ -628,7 +628,6 @@ function stats_start(msg) {
   	}
   	else {
   		isTrackingStats = true;
-  		//weeklyTask.start();
   		msg.respond("Statistics tracking started.");
   	}
   }
@@ -640,7 +639,6 @@ function stats_stop(msg) {
 
 	if(isTrackingStats){
 		isTrackingStats=false;
-		//weeklyTask.stop();
 		msg.respond("Statistics tracking stopped.");
 	}
 	else {
